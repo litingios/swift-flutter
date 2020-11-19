8a81103cbaa09bf702b2dd61e90597fc7b1ed8c8
litingting <1767172804@qq.com>
2020-11-19 11:38:44 +0800

    no message

--- shuqireader/shuqireader/书架/Controller/BookrackViewController.swift
+++ shuqireader/shuqireader/书架/Controller/BookrackViewController.swift
@@ -30,13 +30,23 @@ class BookrackViewController: LTSuperViewController {
         button .addTarget(self, action: #selector(pushMethod), for: .touchUpInside)
         self.view .addSubview(button)
     }
-
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = false
+    }
+
     @objc func pushMethod() -> (Void){
         let flutterVC = FlutterVC ()
         flutterVC.splashScreenView = {
             let view = UIView.init(frame: UIScreen.main.bounds)
-            view.backgroundColor = UIColor.red
+            view.backgroundColor = UIColor.white
+            let textLab = UILabel()
+            textLab.frame = CGRect(x: 0, y: 0, width: ScreenWidth, height: ScreenHeigth)
+            textLab.text = "欢迎来到 flutter 商城模块"
+            textLab.textAlignment = .center
+            textLab.font = UIFont.boldSystemFont(ofSize: 30)
+            view .addSubview(textLab)
             return view
         }()
         flutterVC.navigationItem.title = "商城模块开发"
